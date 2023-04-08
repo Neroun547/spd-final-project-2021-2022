525a89e9ccf6375d6a282c0e56a74f5dc793ad18
Gosha <kovalenko@giria.io>
2023-04-08 10:48:43 +0200

    Delete article-comments feature from master

--- static/js/modules/another-user/another-user-articles/another-user-article.js
+++ static/js/modules/another-user/another-user-articles/another-user-article.js
@@ -11,12 +11,3 @@ link.style.position = "absolute";
 link.style.top = "-15px";
 link.style.left = "20px";
 link.style.fontSize = "45px";
-
-const commentsForm = createElement(document.querySelector(".content__block"), "form", { class: "form__comments" });
-const commentsFormInput = createElement(commentsForm, "textarea", { placeholder: "Write comment:" });
-const commentsFormButton = createElement(commentsForm, "button", { type: "submit" });
-
-commentsFormButton.innerHTML = "Add comment";
-
-const wrapperComments = createElement(document.querySelector(".content__block"), "div", { class: "wrapper__comments" });
-
